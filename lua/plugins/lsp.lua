190e4e71ec7e10ca9000e496fb9fbe23adab2aa9
Simone Gentili <gensimone.git@gmail.com>
2025-03-15 19:44:53 +0100

    Added lsp.lua

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,62 @@
+-- ██╗     ███████╗██████╗
+-- ██║     ██╔════╝██╔══██╗
+-- ██║     ███████╗██████╔╝
+-- ██║     ╚════██║██╔═══╝
+-- ███████╗███████║██║
+-- ╚══════╝╚══════╝╚═╝
+
+return {
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+    config = function()
+      require('mason-lspconfig').setup({
+        -- A list of servers to automatically install
+        ensure_installed = {
+          'clangd',
+          'pyright',
+          'lua_ls',
+          'bashls'
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    keys = {
+      { '<leader>ld', ':LspStop<cr>',  mode = { 'n' }, desc = 'Disable LSP' },
+      { '<leader>le', ':LspStart<cr>', mode = { 'n' }, desc = 'Enable LSP' },
+    },
+    config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local lspconfig = require("lspconfig")
+      lspconfig.clangd.setup({
+        capabilities = capabilities
+      })
+      lspconfig.pyright.setup({
+        capabilities = capabilities
+      })
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.bashls.setup({
+        capabilities = capabilities
+      })
+      -- vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      -- vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      -- vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      -- vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
+}
