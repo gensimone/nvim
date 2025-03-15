f9e1d08696737d3718164f45b93b741adec632c4
Simone Gentili <gensimone.git@gmail.com>
2025-03-15 19:27:21 +0100

    Added lua/options.lua

---
+++ lua/options.lua
@@ -0,0 +1,29 @@
+--  ██████╗ ██████╗ ████████╗██╗ ██████╗ ███╗   ██╗███████╗
+-- ██╔═══██╗██╔══██╗╚══██╔══╝██║██╔═══██╗████╗  ██║██╔════╝
+-- ██║   ██║██████╔╝   ██║   ██║██║   ██║██╔██╗ ██║███████╗
+-- ██║   ██║██╔═══╝    ██║   ██║██║   ██║██║╚██╗██║╚════██║
+-- ╚██████╔╝██║        ██║   ██║╚██████╔╝██║ ╚████║███████║
+--  ╚═════╝ ╚═╝        ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+vim.g.background = "light"
+
+vim.opt.swapfile = false
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.termguicolors = true
+
+-- remove trailing whitespace
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = { "*" },
+  command = [[%s/\s\+$//e]],
+})
+
+
+vim.cmd('set laststatus=0')
